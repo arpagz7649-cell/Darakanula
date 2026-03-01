465bf6e8d07e69b83b4d274d90fd92b05bce58a6
arpagz7649-cell <arpagz7649@gmail.com>
2026-03-01 09:36:11 +0700

    Create MesinInti.cpp

---
+++ MesinInti.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Menambah 2 simbol plus di belakang C
+    string statusKiamat = "LEVEL 5";
+    int jumlahHantu = 666;
+
+    cout << "--- MESIN INTI C++ AKTIF ---" << endl;
+    cout << "Status Dunia: " << statusKiamat << endl;
+    cout << "Anomali terdeteksi di koordinat pemain." << endl;
+
+    return 0;
+}
